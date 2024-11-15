7f3ccf954f0c21dcecdd709fa684aa0323651ee3
Jakob Guldberg Aaes <jakob1379@gmail.com>
2024-11-15 12:20:57 +0100

    initi

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+
+      # Determine the moss script URL based on multiple options
+      mossScriptUrl = if builtins.getEnv "MOSS_SCRIPT_URL" != "" then
+        builtins.getEnv "MOSS_SCRIPT_URL"
+      else if self ? mossUrl then
+        self.mossUrl
+      else
+        "http://example.com/mossnet";
+    in
+    {
+      # Define the moss package as a default package for easier building
+      defaultPackage = pkgs.stdenv.mkDerivation {
+        pname = "moss";
+        version = "1.0.0";
+
+        # Fetch the mossnet Perl script using the determined URL
+        src = pkgs.fetchurl {
+          url = mossScriptUrl;
+          # Keep placeholder sha256; should be replaced if the real script URL is used
+          sha256 = "sha256-FDoWMIIafl93c6orFYbESgR2j8DXzv/3ZddzFPLY3Ns=";
+        };
+
+        # Disable unpackPhase because it's a raw text Perl script
+        dontUnpack = true;
+
+        nativeBuildInputs = [ pkgs.perl ];
+
+        installPhase = ''
+          # Create the output directory for the moss script
+          mkdir -p $out/bin
+          # Copy the mossnet script and rename it to moss for easier usage
+          cp $src $out/bin/moss
+          chmod +x $out/bin/moss
+        '';
+
+        meta = with pkgs.lib; {
+          description = "MOSS plagiarism detection script";
+          license = licenses.unfreeRedistributable; # Not freely licensed
+          maintainers = [ maintainers.jakob1379 ];
+          platforms = platforms.all;
+        };
+      };
+
+      # Optional: keep moss available as an explicit package reference
+      packages.moss = self.defaultPackage;
+    });
+}
